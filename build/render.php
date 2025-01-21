27353a776c5ea4766e03ddcaecc5a3f22d658a77
imath <math.viet@gmail.com>
2025-01-21 23:29:17 +0100

    Generate the block content according to current locale

--- build/render.php
+++ build/render.php
@@ -1,1 +1,22 @@
 <?php
+
+if ( ! isset( $block->parsed_block['innerBlocks'] ) ) {
+	return;
+}
+
+$locale          = strtolower( str_replace( '_', '-', get_locale() ) );
+$dubbed_contents = $block->parsed_block['innerBlocks'];
+
+foreach ( $dubbed_contents as $dubbed_content ) {
+	if ( ! isset( $dubbed_content['attrs']['language'] ) || $locale !== $dubbed_content['attrs']['language'] || ! isset( $dubbed_content['innerBlocks'] ) ) {
+		continue;
+	}
+
+	foreach ( $dubbed_content['innerBlocks'] as $inner_block ) {
+		if ( ! isset( $inner_block['innerHTML'] ) ) {
+			continue;
+		}
+
+		echo $inner_block['innerHTML'];
+	}
+}
