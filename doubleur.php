78f6d23648efeaff1465d1e3a6ace0ed46793873
imath <math.viet@gmail.com>
2025-02-15 10:57:06 +0100

    Bump Block stable version to 1.1.0

--- doubleur.php
+++ doubleur.php
@@ -12,7 +12,7 @@
  * Plugin URI:         https://github.com/imath/doubleur
  * Plugin Type:        block
  * Description:        Authors can use this block to dub the post or page content in english.
- * Version:            1.0.0
+ * Version:            1.1.0
  * Author:             imath
  * Author URI:         https://imathi.eu
  * Requires Retraceur: 1.0.0-beta1
