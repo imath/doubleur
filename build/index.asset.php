3f137b28770d5d7a9f52711d0ce29b8297945df7
imath <math.viet@gmail.com>
2025-03-06 01:38:41 +0100

    Update build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '0da7f0ad9cc3a96638b4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => 'cbab753767f7043e97e9');
