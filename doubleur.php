af76e62d7631a6b5b14608207a15a2a04147d2b2
imath <math.viet@gmail.com>
2026-07-23 11:16:06 +0200

    Prepare first 1.3.0 beta
    - Add the Retraceur’s manifest file
    - Update Changelog
    - Do Retraceur & PHP required version bumps
    
    See #5

--- doubleur.php
+++ doubleur.php
@@ -12,12 +12,12 @@
  * Plugin URI:         https://github.com/imath/doubleur
  * Plugin Type:        block
  * Description:        Authors can use this block to dub the post or page content in english.
- * Version:            1.3.0-alpha
+ * Version:            1.3.0-beta1
  * Author:             imath
  * Author URI:         https://imathi.eu
- * Requires Retraceur: 1.0.0
+ * Requires Retraceur: 3.2.0
  * Up to Retraceur:    4.0.0
- * Requires PHP:       5.6
+ * Requires PHP:       7.4
  * Text Domain:        doubleur
  * License:            MIT License
  * License URI:        https://github.com/imath/doubleur/blob/master/LICENSE.md
